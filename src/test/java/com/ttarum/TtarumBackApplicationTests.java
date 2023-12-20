0c9101f95169e1e9021d4fc2e8170a3ede959a00
koowin42@gmail.com <koowin42@gmail.com>
2023-12-20 21:59:19 +0900

    initialize spring boot project

---
+++ src/test/java/com/ttarum/TtarumBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttarum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtarumBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

368a3f875312e940ed5b5977e9580590ff32c408
koowin42@gmail.com <koowin42@gmail.com>
2024-01-01 17:25:57 +0900

    TTAR-43 Remove the field `updatedAt` in BaseEntity

--- src/main/java/com/ttarum/common/domain/BaseEntity.java
+++ src/main/java/com/ttarum/common/domain/BaseEntity.java
@@ -13,18 +13,9 @@ public class BaseEntity {
     @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
 
-    @Column(name = "updated_at", nullable = false)
-    private Instant updatedAt;
-
     @PrePersist
     public void prePersist() {
         Instant now = Instant.now();
         this.createdAt = now;
-        this.updatedAt = now;
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        this.updatedAt = Instant.now();
     }
 }

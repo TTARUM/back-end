9d47fb47d3ca2bd6e43958e290ae79cdce0d3985
salbyul <kpast96@gmail.com>
2024-01-06 19:27:06 +0900

    added method getItem to get Item Entity

--- src/main/java/com/ttarum/item/service/ItemService.java
+++ src/main/java/com/ttarum/item/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.ttarum.item.service;
 
+import com.ttarum.item.domain.Item;
 import com.ttarum.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,4 +12,13 @@ import org.springframework.stereotype.Service;
 public class ItemService {
 
     private final ItemRepository itemRepository;
+
+    public Item getItem(final Long id) {
+        return getItemById(id);
+    }
+
+    private Item getItemById(final Long id) {
+        return itemRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("아이템이 존재하지 않습니다."));
+    }
 }
---
+++ src/test/java/com/ttarum/item/service/ItemServiceTest.java
@@ -0,0 +1,58 @@
+package com.ttarum.item.service;
+
+import com.ttarum.item.domain.Item;
+import com.ttarum.item.repository.ItemRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class ItemServiceTest {
+
+    @InjectMocks
+    private ItemService itemService;
+
+    @Mock
+    private ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("아이템의 아이디로 아이템을 찾을 수 있다.")
+    void getItem() {
+        Item item = Item.builder()
+                .id(1L)
+                .name("와인")
+                .description("와인 설명")
+                .price(13000)
+                .itemImageUrl("/Home/image/image/imageName.jpg")
+                .itemDescriptionImageUrl("/Home/image/description/imageName.jpg")
+                .build();
+        doReturn(Optional.of(item)).when(itemRepository).findById(1L);
+
+        Item foundItem = itemService.getItem(1L);
+
+        assertThat(foundItem.getId()).isEqualTo(item.getId());
+        assertThat(foundItem.getName()).isEqualTo(item.getName());
+        assertThat(foundItem.getDescription()).isEqualTo(item.getDescription());
+        assertThat(foundItem.getPrice()).isEqualTo(item.getPrice());
+        assertThat(foundItem.getItemImageUrl()).isEqualTo(item.getItemImageUrl());
+        assertThat(foundItem.getItemDescriptionImageUrl()).isEqualTo(item.getItemDescriptionImageUrl());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 아이템을 찾을 경우 예외가 발생한다.")
+    void getItemFailureByInvalidId() {
+        doReturn(Optional.empty()).when(itemRepository).findById(0L);
+
+        assertThatThrownBy(() -> itemService.getItem(0L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("아이템이 존재하지 않습니다.");
+    }
+}

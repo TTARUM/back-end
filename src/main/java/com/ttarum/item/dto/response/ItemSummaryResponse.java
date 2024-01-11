7b47eff2da113db440a91fb8b5f1aacede95e4d5
salbyul <kpast96@gmail.com>
2024-01-11 15:11:37 +0900

    Created ItemSummaryResponse DTO

---
+++ src/main/java/com/ttarum/item/dto/response/ItemSummaryResponse.java
@@ -0,0 +1,19 @@
+package com.ttarum.item.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class ItemSummaryResponse {
+
+    private final String categoryName;
+    private final String name;
+    private final int price;
+    private final double rating;
+    private final String imageUrl;
+}

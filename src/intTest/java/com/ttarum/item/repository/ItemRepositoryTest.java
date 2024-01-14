20ebfd812da19f5fcbecf7642a5b2a629e9e3aa4
salbyul <kpast96@gmail.com>
2024-01-14 20:17:08 +0900

    Moved ItemRepositoryTest in test directory to intTest

--- src/intTest/java/com/ttarum/item/repository/ItemRepositoryTest.java
+++ src/intTest/java/com/ttarum/item/repository/ItemRepositoryTest.java
@@ -3,10 +3,10 @@ package com.ttarum.item.repository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 
-@SpringBootTest
+@DataJpaTest
 class ItemRepositoryTest {
 
     @Autowired

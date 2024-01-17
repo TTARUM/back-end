ea74b32d712434325240e0e6ecdf6198b88f4aeb
koowin42@gmail.com <koowin42@gmail.com>
2024-01-17 20:15:32 +0900

    TTAR-58 Add swagger descriptions

--- src/main/java/com/ttarum/member/dto/request/NormalMemberRegister.java
+++ src/main/java/com/ttarum/member/dto/request/NormalMemberRegister.java
@@ -2,15 +2,25 @@ package com.ttarum.member.dto.request;
 
 import com.ttarum.member.domain.Member;
 import com.ttarum.member.domain.NormalMember;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
+@Schema(name = "NormalMemberRegister", description = "일반유저 회원가입 DTO")
 public class NormalMemberRegister {
+    @Schema(description = "이름", example = "홍길동")
     private String name;
+    @Schema(description = "닉네임", example = "와인조아")
     private String nickname;
+    @Schema(description = "전화번호", example = "010-1234-5678")
     private String phoneNumber;
+    @Schema(description = "로그인 아이디", example = "ttarum")
     private String loginId;
+    @Schema(description = "비밀번호", example = "password1234")
     private String password;
+    @Schema(description = "이메일", example = "ttarum@gmail.com")
     private String email;
 
     public Member toMemberEntity() {

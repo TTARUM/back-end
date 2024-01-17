6d41e425de035a2523396cfd6b4ce588774cfbaa
salbyul <kpast96@gmail.com>
2024-01-17 14:42:13 +0900

    Created findReviewResponseByItemId Method

---
+++ src/intTest/java/com/ttarum/review/repository/ReviewRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.ttarum.review.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class ReviewRepositoryTest {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Test
+    void findReviewResponseByItemId() {
+        reviewRepository.findReviewResponseByItemId(1L);
+    }
+}
--- src/main/java/com/ttarum/review/repository/ReviewRepository.java
+++ src/main/java/com/ttarum/review/repository/ReviewRepository.java
@@ -1,7 +1,23 @@
 package com.ttarum.review.repository;
 
 import com.ttarum.review.domain.Review;
+import com.ttarum.review.dto.response.ReviewResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("""
+            SELECT new com.ttarum.review.dto.response.ReviewResponse(m.nickname, r.content, r.star, ri.fileUrl, r.createdAt)
+            FROM Review r
+            LEFT JOIN FETCH ReviewImage ri
+            ON ri.review.id = r.id
+            LEFT JOIN FETCH Member m
+            ON m.id = r.member.id
+            WHERE r.item.id = :itemId
+            """)
+    List<ReviewResponse> findReviewResponseByItemId(@Param("itemId") Long itemId);
 }

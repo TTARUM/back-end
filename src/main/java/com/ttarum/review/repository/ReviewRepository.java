096e23eec266901662555bb26a1a08c5d84c5b46
salbyul <kpast96@gmail.com>
2024-01-17 16:30:05 +0900

    The findReviewResponseByItemId query was separated into queries that find Review and ReviewImage respectively.

--- src/main/java/com/ttarum/review/repository/ReviewRepository.java
+++ src/main/java/com/ttarum/review/repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.ttarum.review.repository;
 
 import com.ttarum.review.domain.Review;
+import com.ttarum.review.domain.ReviewImage;
 import com.ttarum.review.dto.response.ReviewResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,13 +12,18 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("""
-            SELECT new com.ttarum.review.dto.response.ReviewResponse(m.nickname, r.content, r.star, ri.fileUrl, r.createdAt)
+            SELECT new com.ttarum.review.dto.response.ReviewResponse(r.id, m.nickname, r.content, r.star, r.createdAt)
             FROM Review r
-            LEFT JOIN FETCH ReviewImage ri
-            ON ri.review.id = r.id
             LEFT JOIN FETCH Member m
             ON m.id = r.member.id
             WHERE r.item.id = :itemId
             """)
     List<ReviewResponse> findReviewResponseByItemId(@Param("itemId") Long itemId);
+
+    @Query("""
+            SELECT ri
+            FROM ReviewImage ri
+            WHERE ri.review.id IN (:reviewIds)
+            """)
+    List<ReviewImage> findReviewImageByReviewId(@Param("reviewIds") List<Long> reviewIds);
 }

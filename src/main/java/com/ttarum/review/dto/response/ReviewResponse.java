38b9dc6dc39eeb7078098ad66cb02aa05bfda0ba
salbyul <kpast96@gmail.com>
2024-01-17 16:33:18 +0900

    Changed type of imageUrl field to Queue<ReviewImageResponse>

--- src/main/java/com/ttarum/review/dto/response/ReviewResponse.java
+++ src/main/java/com/ttarum/review/dto/response/ReviewResponse.java
@@ -1,17 +1,30 @@
 package com.ttarum.review.dto.response;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.Instant;
+import java.util.*;
 
 @Getter
-@AllArgsConstructor
+@Builder
 public class ReviewResponse {
+    private final Long id;
     private final String nickname;
     private final String content;
     private final short rating;
-    private final String imageUrl;
+    private final Queue<ReviewImageResponse> imageUrls = new PriorityQueue<>(Comparator.comparingInt(ReviewImageResponse::getOrder));
     private final Instant createdAt;
 
+    public ReviewResponse(final Long id, final String nickname, final String content, final short rating, final Instant createdAt) {
+        this.id = id;
+        this.nickname = nickname;
+        this.content = content;
+        this.rating = rating;
+        this.createdAt = createdAt;
+    }
+
+    public void addImageUrl(final ReviewImageResponse reviewImage) {
+        imageUrls.add(reviewImage);
+    }
 }

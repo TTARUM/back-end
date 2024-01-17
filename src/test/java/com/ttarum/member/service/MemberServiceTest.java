7c3afc25a85b75bd801079abeb71aa768f52467c
koowin42@gmail.com <koowin42@gmail.com>
2024-01-17 20:37:16 +0900

    TTAR-58 Add tests

--- src/test/java/com/ttarum/member/service/MemberServiceTest.java
+++ src/test/java/com/ttarum/member/service/MemberServiceTest.java
@@ -8,6 +8,8 @@ import com.ttarum.member.repository.NormalMemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -33,13 +35,13 @@ public class MemberServiceTest {
     void registerNormalUser() {
         // given
         Member targetMember = Member.builder()
-                .nickname("testNickname")
+                .nickname("nickname1")
                 .name("testName")
                 .phoneNumber("testPhoneNumber")
                 .build();
         NormalMember targetNormalMember = NormalMember.builder()
                 .loginId("testLoginId")
-                .password("testPassword")
+                .password("testPassword123")
                 .email("testEmail@gmail.com")
                 .build();
 
@@ -49,6 +51,81 @@ public class MemberServiceTest {
         // then
         verify(memberRepository, times(1)).save(targetMember);
         verify(normalMemberRepository, times(1)).save(targetNormalMember);
+    }
+
+    @ParameterizedTest
+    @DisplayName("일반 유저 회원가입 - 유효하지 않은 닉네임으로 가입 시 예외가 발생한다.")
+    @ValueSource(strings = {"", "tooLongNickname"})
+    void registerNormalUser_registerWithInvalidNicknameThrowException(String nickname) {
+        // given
+        Member targetMember = Member.builder()
+                .nickname(nickname)
+                .name("testName")
+                .phoneNumber("testPhoneNumber")
+                .build();
+        NormalMember targetNormalMember = NormalMember.builder()
+                .loginId("testLoginId")
+                .password("testPassword123")
+                .email("testEmail@gmail.com")
+                .build();
+
+        // when
+        Exception exception = assertThrows(MemberException.class, () -> {
+            memberService.registerNormalUser(targetMember, targetNormalMember);
+        });
+
+        // then
+        assertTrue(exception.getMessage().contains("올바르지 않은 닉네임입니다."));
+    }
+
+    @ParameterizedTest
+    @DisplayName("일반 유저 회원가입 - 유효하지 않은 이메일로 가입 시 예외가 발생한다.")
+    @ValueSource(strings = {"", "noDomain", "noAtSign.com", "noDot@com"})
+    void registerNormalUser_registerWithInvalidEmailThrowException(String email) {
+        // given
+        Member targetMember = Member.builder()
+                .nickname("nickname1")
+                .name("testName")
+                .phoneNumber("testPhoneNumber")
+                .build();
+        NormalMember targetNormalMember = NormalMember.builder()
+                .loginId("testLoginId")
+                .password("testPassword123")
+                .email(email)
+                .build();
+
+        // when
+        Exception exception = assertThrows(MemberException.class, () -> {
+            memberService.registerNormalUser(targetMember, targetNormalMember);
+        });
+
+        // then
+        assertTrue(exception.getMessage().contains("올바르지 않은 이메일입니다."));
+    }
+
+    @ParameterizedTest
+    @DisplayName("일반 유저 회원가입 - 유효하지 않은 비밀번호로 가입 시 예외가 발생한다.")
+    @ValueSource(strings = {"", "short", "noNumber", "12341234"})
+    void registerNormalUser_registerWithInvalidPasswordThrowException(String password) {
+        // given
+        Member targetMember = Member.builder()
+                .nickname("nickname1")
+                .name("testName")
+                .phoneNumber("testPhoneNumber")
+                .build();
+        NormalMember targetNormalMember = NormalMember.builder()
+                .loginId("testLoginId")
+                .password(password)
+                .email("testEmail@gmail.com")
+                .build();
+
+        // when
+        Exception exception = assertThrows(MemberException.class, () -> {
+            memberService.registerNormalUser(targetMember, targetNormalMember);
+        });
+
+        // then
+        assertTrue(exception.getMessage().contains("올바르지 않은 비밀번호입니다."));
     }
 
     @Test
@@ -62,7 +139,7 @@ public class MemberServiceTest {
                 .build();
         NormalMember targetNormalMember = NormalMember.builder()
                 .loginId("testLoginId")
-                .password("testPassword")
+                .password("testPassword123")
                 .email("testEmail@gmail.com")
                 .build();
 
@@ -88,7 +165,7 @@ public class MemberServiceTest {
                 .build();
         NormalMember targetNormalMember = NormalMember.builder()
                 .loginId("testLoginId")
-                .password("testPassword")
+                .password("testPassword123")
                 .email("testEmail@gmail.com")
                 .build();
 

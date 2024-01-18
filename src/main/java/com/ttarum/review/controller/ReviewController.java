4b6a1145e3ffebeffd484f4816034f22099ec2d5
salbyul <kpast96@gmail.com>
2024-01-18 13:53:42 +0900

    Added getReviewResponseList method

--- src/main/java/com/ttarum/review/controller/ReviewController.java
+++ src/main/java/com/ttarum/review/controller/ReviewController.java
@@ -1,7 +1,27 @@
 package com.ttarum.review.controller;
 
+import com.ttarum.common.annotation.VerificationUser;
+import com.ttarum.common.dto.user.User;
+import com.ttarum.review.dto.response.ReviewResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Tag(name = "review", description = "리뷰")
 public interface ReviewController {
+
+    @Operation(summary = "특정 제품에 대한 리뷰 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "404", description = "유효하지 않은 제품 조회")
+    })
+    @Parameter(name = "itemId", description = "제품의 PK 값", example ="1")
+    @GetMapping
+    ResponseEntity<List<ReviewResponse>> getReviewResponseList(final Long itemId, @VerificationUser final User user);
 }
--- src/main/java/com/ttarum/review/controller/ReviewControllerImpl.java
+++ src/main/java/com/ttarum/review/controller/ReviewControllerImpl.java
@@ -1,11 +1,35 @@
 package com.ttarum.review.controller;
 
+import com.ttarum.common.annotation.VerificationUser;
+import com.ttarum.common.dto.user.User;
+import com.ttarum.review.dto.response.ReviewResponse;
+import com.ttarum.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/reviews")
 public class ReviewControllerImpl implements ReviewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping
+    @Override
+    public ResponseEntity<List<ReviewResponse>> getReviewResponseList(final Long itemId, @VerificationUser final User user) {
+        List<ReviewResponse> list;
+        if (user.isLoggedIn()) {
+            list = reviewService.getReviewResponseList(itemId, user.getId());
+        } else {
+            list = reviewService.getReviewResponseList(itemId);
+        }
+        return ResponseEntity.ok(list);
+    }
 }

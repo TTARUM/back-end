7c21f77428ed2af59f56a5c0959299e9c65ddf63
salbyul <kpast96@gmail.com>
2024-01-28 20:26:30 +0900

    Added Hidden annotation

--- src/main/java/com/ttarum/review/controller/advice/ReviewControllerAdvice.java
+++ src/main/java/com/ttarum/review/controller/advice/ReviewControllerAdvice.java
@@ -2,6 +2,7 @@ package com.ttarum.review.controller.advice;
 
 import com.ttarum.common.dto.response.error.ErrorResponse;
 import com.ttarum.review.exception.ReviewException;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
+@Hidden
 @RestControllerAdvice(basePackages = "com.ttarum.review")
 public class ReviewControllerAdvice {
 

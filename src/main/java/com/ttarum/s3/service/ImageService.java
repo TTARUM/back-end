bfdaa034c88bc9c43eb04a2b3f8df7c2ea7dea28
koowin42@gmail.com <koowin42@gmail.com>
2024-02-01 02:43:06 +0900

    TTAR-71 Add ImageService

---
+++ src/main/java/com/ttarum/s3/service/ImageService.java
@@ -0,0 +1,23 @@
+package com.ttarum.s3.service;
+
+import com.ttarum.s3.component.S3Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URL;
+
+@Service
+public class ImageService {
+    private final S3Component s3Component;
+
+    public ImageService(S3Component s3Component) {
+        this.s3Component = s3Component;
+    }
+
+    public URL saveImage(MultipartFile file) throws IOException {
+        byte[] imageBytes = file.getBytes();
+        String originalFilename = file.getOriginalFilename();
+        return s3Component.uploadImage(imageBytes, originalFilename);
+    }
+}

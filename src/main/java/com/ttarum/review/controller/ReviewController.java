9ad20cda83b4e9455c4f443c3dd89a3fd6ef6ed9
salbyul <kpast96@gmail.com>
2024-02-02 02:41:36 +0900

    Added docs

--- src/main/java/com/ttarum/review/controller/ReviewController.java
+++ src/main/java/com/ttarum/review/controller/ReviewController.java
@@ -45,12 +45,19 @@ public interface ReviewController {
                                                                final Optional<Integer> page,
                                                                final Optional<Integer> size);
 
+    /**
+     * 리뷰 제거
+     *
+     * @param optionalReviewId 제거할 리뷰의 ID
+     * @param user             로그인한 유저
+     * @return
+     */
     @Operation(summary = "특정 리뷰 제거")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "제거 성공"),
             @ApiResponse(responseCode = "400", description = "유효하지 않은 리뷰")
     })
-    @Parameter(name = "reviewId", description = "리뷰의 Id 값", example = "1")
+    @Parameter(name = "reviewId", description = "리뷰의 ID 값", example = "1")
     @DeleteMapping
     ResponseEntity<Object> deleteReview(@RequestParam(name = "reviewId") final Optional<Long> optionalReviewId, @AuthenticationPrincipal final UserDetail user);
 

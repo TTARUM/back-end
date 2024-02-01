90474b4f3ad6617dab45b4daadb9b4488702c007
salbyul <kpast96@gmail.com>
2024-02-02 01:13:11 +0900

    Deleted unnecessary test and changed method name 'getEmptyReviewResponseListByInvalidItemId' to 'getReviewResponseListFailureByInvalidItemId'

--- src/test/java/com/ttarum/review/service/ReviewServiceTest.java
+++ src/test/java/com/ttarum/review/service/ReviewServiceTest.java
@@ -71,42 +71,8 @@ class ReviewServiceTest {
     }
 
     @Test
-    @DisplayName("제품의 PK 값으로 리뷰 목록을 조회할 수 있다.")
-    void getReviewResponseListWithMemberId() {
-        // given
-        List<ReviewResponse> reviewResponseList = new ArrayList<>();
-        reviewResponseList.add(ReviewResponse.builder().id(1L).build());
-        reviewResponseList.add(ReviewResponse.builder().id(2L).build());
-
-        List<ReviewImage> reviewImageList = new ArrayList<>();
-        reviewImageList.add(ReviewImage.builder().review(Review.builder().id(1L).build()).order(1).build());
-        reviewImageList.add(ReviewImage.builder().review(Review.builder().id(2L).build()).order(1).build());
-        reviewImageList.add(ReviewImage.builder().review(Review.builder().id(2L).build()).order(2).build());
-        reviewImageList.add(ReviewImage.builder().review(Review.builder().id(2L).build()).order(3).build());
-        PageRequest pageRequest = PageRequest.of(0, 10);
-
-
-        // when
-        when(reviewRepository.findReviewResponseByItemId(anyLong(), any(Pageable.class))).thenReturn(reviewResponseList);
-        when(reviewRepository.findReviewImageByReviewId(anyList())).thenReturn(reviewImageList);
-        when(itemRepository.findById(1L)).thenReturn(Optional.of(Item.builder().id(1L).build()));
-
-        List<ReviewResponse> result = reviewService.getReviewResponseList(1L, pageRequest);
-
-        // then
-        verify(reviewRepository, times(1)).findReviewResponseByItemId(anyLong(), any(Pageable.class));
-        verify(reviewRepository, times(1)).findReviewImageByReviewId(anyList());
-
-        assertThat(result).hasSize(2);
-        assertThat(result.get(0).getId()).isEqualTo(1L);
-        assertThat(result.get(1).getId()).isEqualTo(2L);
-        assertThat(result.get(0).getImageUrls()).hasSize(1);
-        assertThat(result.get(1).getImageUrls()).hasSize(3);
-    }
-
-    @Test
     @DisplayName("올바르지 않은 제품의 PK 값을 받으면 예외가 발생한다.")
-    void getEmptyReviewResponseListByInvalidItemId() {
+    void getReviewResponseListFailureByInvalidItemId() {
         PageRequest pageRequest = PageRequest.of(0, 10);
 
         assertThatThrownBy(() -> reviewService.getReviewResponseList(-1L, pageRequest))

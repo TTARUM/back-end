4bb4eb3f29e158fa10e10afa8bdcdf3b9078fc1a
salbyul <kpast96@gmail.com>
2024-02-02 21:44:34 +0900

    Added Builder annotation

--- src/main/java/com/ttarum/review/dto/request/ReviewUpdateRequest.java
+++ src/main/java/com/ttarum/review/dto/request/ReviewUpdateRequest.java
@@ -1,9 +1,11 @@
 package com.ttarum.review.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class ReviewUpdateRequest {
 

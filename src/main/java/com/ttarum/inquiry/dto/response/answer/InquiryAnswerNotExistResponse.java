cf76b240f06439b22cecbe97e2670baadb2abc0a
salbyul <kpast96@gmail.com>
2024-02-05 16:17:42 +0900

    Modified NOT_EXISTS_MESSAGE field access type 'public' to 'private'

--- src/main/java/com/ttarum/inquiry/dto/response/answer/InquiryAnswerNotExistResponse.java
+++ src/main/java/com/ttarum/inquiry/dto/response/answer/InquiryAnswerNotExistResponse.java
@@ -2,7 +2,7 @@ package com.ttarum.inquiry.dto.response.answer;
 
 public class InquiryAnswerNotExistResponse extends InquiryAnswerResponse {
 
-    public static final String NOT_EXISTS_MESSAGE = "답변이 존재하지 않습니다.";
+    private static final String NOT_EXISTS_MESSAGE = "답변이 존재하지 않습니다.";
 
     public InquiryAnswerNotExistResponse(final String content) {
         super(content);

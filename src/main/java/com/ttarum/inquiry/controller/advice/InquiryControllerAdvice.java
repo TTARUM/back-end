acb37eda030aafdd37be97c77762eaafa00399f5
salbyul <kpast96@gmail.com>
2024-02-05 17:12:16 +0900

    Added Hidden Annotation

--- src/main/java/com/ttarum/inquiry/controller/advice/InquiryControllerAdvice.java
+++ src/main/java/com/ttarum/inquiry/controller/advice/InquiryControllerAdvice.java
@@ -2,6 +2,7 @@ package com.ttarum.inquiry.controller.advice;
 
 import com.ttarum.common.dto.response.error.ErrorResponse;
 import com.ttarum.inquiry.exception.InquiryException;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
+@Hidden
 @RestControllerAdvice(basePackages = {"com.ttarum.inquiry"})
 public class InquiryControllerAdvice {
 

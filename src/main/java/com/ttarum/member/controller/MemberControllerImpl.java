3af35a2108f01e9df669433dfff38977b8bbd8de
koowin42@gmail.com <koowin42@gmail.com>
2024-02-12 19:50:09 +0900

    TTAR-81 Add an endpoint for register api

--- src/main/java/com/ttarum/member/controller/MemberControllerImpl.java
+++ src/main/java/com/ttarum/member/controller/MemberControllerImpl.java
@@ -4,6 +4,7 @@ import com.ttarum.member.dto.request.NormalMemberRegister;
 import com.ttarum.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberControllerImpl implements MemberController {
     private final MemberService memberService;
     @Override
+    @PostMapping("/register")
     public void registerNormalMember(NormalMemberRegister dto) {
         memberService.registerNormalUser(dto.toMemberEntity(), dto.toNormalMemberEntity());
     }

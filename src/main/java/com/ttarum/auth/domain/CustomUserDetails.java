342686b13668738baeddf620ec433861adc0a53e
koowin42@gmail.com <koowin42@gmail.com>
2024-02-14 14:09:25 +0900

    TTAR-84 Rename class

--- src/main/java/com/ttarum/auth/domain/CustomUserDetails.java
+++ src/main/java/com/ttarum/auth/domain/CustomUserDetails.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 // fixme
-public class UserDetail implements UserDetails {
+public class CustomUserDetails implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
--- src/main/java/com/ttarum/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/ttarum/auth/service/CustomUserDetailsService.java
@@ -1,13 +1,17 @@
 package com.ttarum.auth.service;
 
+import com.ttarum.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 // fixme
+@RequiredArgsConstructor
 @Service
-public class UserDetailService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         return null;

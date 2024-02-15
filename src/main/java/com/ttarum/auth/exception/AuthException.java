2de352d105540d45ffea23f73953ff5345582db0
salbyul <kpast96@gmail.com>
2024-02-15 21:45:38 +0900

    The exception for Auth has been extended to the 'TtarumException' exception.

--- src/main/java/com/ttarum/auth/exception/AuthException.java
+++ src/main/java/com/ttarum/auth/exception/AuthException.java
@@ -1,5 +1,11 @@
 package com.ttarum.auth.exception;
 
-public class AuthException extends RuntimeException {
-    public AuthException(final String message) { super(message); }
+import com.ttarum.common.exception.TtarumException;
+import org.springframework.http.HttpStatusCode;
+
+public class AuthException extends TtarumException {
+
+    public AuthException(final HttpStatusCode status, final String message) {
+        super(status, message);
+    }
 }

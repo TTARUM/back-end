5da185e102bad8544fa65c2b8c7ae41b21a9113d
koowin42@gmail.com <koowin42@gmail.com>
2024-02-18 19:20:43 +0900

    TTAR-93 Remove Transactional annotation

--- src/main/java/com/ttarum/auth/service/AuthService.java
+++ src/main/java/com/ttarum/auth/service/AuthService.java
@@ -7,7 +7,6 @@ import com.ttarum.auth.exception.AuthException;
 import com.ttarum.member.domain.Member;
 import com.ttarum.member.domain.NormalMember;
 import com.ttarum.member.repository.NormalMemberRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -20,7 +19,6 @@ public class AuthService {
     private final NormalMemberRepository normalMemberRepository;
     private final JwtUtil jwtUtil;
 
-    @Transactional
     public LoginResponse normalLogin(final NormalLoginRequest dto) {
         Optional<NormalMember> normalMember = normalMemberRepository.findNormalMemberByLoginId(dto.getLoginId());
         if (normalMember.isEmpty()) {

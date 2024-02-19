b0fa978a08c675efd1ace18d965cab6909b9f710
salbyul <kpast96@gmail.com>
2024-02-19 16:02:37 +0900

    Added @Query annotation to 'findByMemberIdAndItemId' method.

--- src/main/java/com/ttarum/member/repository/WishListRepository.java
+++ src/main/java/com/ttarum/member/repository/WishListRepository.java
@@ -3,10 +3,13 @@ package com.ttarum.member.repository;
 import com.ttarum.member.domain.WishList;
 import com.ttarum.member.domain.WishListId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface WishListRepository extends JpaRepository<WishList, WishListId> {
 
-    Optional<WishList> findByMemberIdAndItemId(long memberId, long itemId);
+    @Query("SELECT wl FROM WishList wl WHERE wl.item.id = :itemId AND wl.member.id = :memberId")
+    Optional<WishList> findByMemberIdAndItemId(@Param("memberId") long memberId, @Param("itemId") long itemId);
 }

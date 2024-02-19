1f01d15596eaf521fc5bcce642f7d6d423e31f5e
salbyul <kpast96@gmail.com>
2024-02-19 14:19:31 +0900

    Removed parameters in 'postInquiry' swagger docs.

--- src/main/java/com/ttarum/inquiry/controller/InquiryController.java
+++ src/main/java/com/ttarum/inquiry/controller/InquiryController.java
@@ -86,18 +86,6 @@ public interface InquiryController {
             @ApiResponse(responseCode = "200", description = "문의글 등록 성공"),
             @ApiResponse(responseCode = "400", description = "문의글 등록 실패")
     })
-    @Parameters(value = {
-            @Parameter(name = "inquiryRequest", description = "등록할 문의글", required = true, example = """
-                    {
-                        "title": "문의글 제목",
-                        "content": "문의글 내용",
-                        "isSecret": false,
-                        "itemId": 1
-                    }
-                    """),
-            @Parameter(name = "images", description = "이미지 파일들"),
-            @Parameter(name = "user", hidden = true)
-    })
     @Transactional
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ResponseEntity<InquiryCreationResponse> postInquiry(@RequestPart(name = "inquiryRequest") InquiryCreationRequest request,

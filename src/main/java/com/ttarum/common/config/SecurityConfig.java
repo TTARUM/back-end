50c0d022c1b9954e34332f6c3895c6c3719ab12d
salbyul <kpast96@gmail.com>
2024-02-21 16:24:36 +0900

    Three endpoints have been added to allow access by anonymous users.

--- src/main/java/com/ttarum/common/config/SecurityConfig.java
+++ src/main/java/com/ttarum/common/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.ttarum.auth.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,13 +39,15 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/items/list").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/inquiries/list").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/inquiries").permitAll()
                         .anyRequest().authenticated()
                 );
 
         http
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(exceptionHandlerFilter, JwtAuthenticationFilter.class);
-
         return http.build();
     }
 

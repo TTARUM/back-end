b7ff211686cc13575457baa683ea4aba20e29580
koowin42@gmail.com <koowin42@gmail.com>
2024-02-25 18:57:28 +0900

    TTAR-068 Add DTO `AddressAdditionRequest`

---
+++ src/main/java/com/ttarum/member/dto/request/AddressAdditionRequest.java
@@ -0,0 +1,13 @@
+package com.ttarum.member.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Schema(name = "AddressAdditionRequest", description = "배송지 추가 요청 DTO")
+public class AddressAdditionRequest {
+    @Schema(description = "배송지 주소", example = "서울시 강남구 역삼동 123-456")
+    private final String address;
+}

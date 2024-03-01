c728ae7521ef8c279dcfb899d4db309468e45482
koowin42@gmail.com <koowin42@gmail.com>
2024-03-01 17:41:17 +0900

    TTAR-080 Add integration test for purchase item

--- src/intTest/java/com/ttarum/item/service/RedisServiceTest.java
+++ src/intTest/java/com/ttarum/item/service/RedisServiceTest.java
@@ -40,4 +40,32 @@ public class RedisServiceTest {
 
         redisService.deleteAllSearchKeywords();
     }
+
+    @Test
+    void testIncreasePurchaseCount() {
+        // given
+        Long itemId1 = 1L;
+        Long itemId2 = 2L;
+        Long itemId3 = 3L;
+
+        // when
+        redisService.increasePurchaseCount(itemId1);
+
+        redisService.increasePurchaseCount(itemId3);
+        redisService.increasePurchaseCount(itemId3);
+        redisService.increasePurchaseCount(itemId3);
+
+        redisService.increasePurchaseCount(itemId2);
+        redisService.increasePurchaseCount(itemId2);
+
+        List<Long> list = redisService.getPopularPurchaseItems(3);
+
+        // then
+        assertEquals(3, list.size());
+        assertEquals(itemId3, list.get(0));
+        assertEquals(itemId2, list.get(1));
+        assertEquals(itemId1, list.get(2));
+
+        redisService.deleteAllPurchaseItems();
+    }
 }

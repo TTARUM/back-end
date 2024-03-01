4b4d5ac6d61c3fd24bef232363127a4ca8951b63
koowin42@gmail.com <koowin42@gmail.com>
2024-03-01 17:40:16 +0900

    TTAR-080 Add redis methods for product purchase

--- src/main/java/com/ttarum/item/service/RedisService.java
+++ src/main/java/com/ttarum/item/service/RedisService.java
@@ -13,6 +13,7 @@ import java.util.Set;
 public class RedisService {
     private final StringRedisTemplate redisTemplate;
     private final String SEARCH_KEYWORD_ZSET_KEY = "searchKeywords";
+    private final String PURCHASE_ITEM_ZSET_KEY = "purchaseItems";
 
     /**
      * 검색 키워드 카운트 증가
@@ -42,5 +43,38 @@ public class RedisService {
      */
     public void deleteAllSearchKeywords() {
         redisTemplate.delete(SEARCH_KEYWORD_ZSET_KEY);
+    }
+
+    /**
+     * 상품 구매 카운트 증가
+     *
+     * @param itemId 상품 아이디
+     */
+    public void increasePurchaseCount(Long itemId) {
+        redisTemplate.opsForZSet().incrementScore(PURCHASE_ITEM_ZSET_KEY, itemId.toString(), 1);
+    }
+
+    /**
+     * 인기 상품 목록 조회
+     *
+     * @param topN 조회할 인기 상품 개수
+     * @return 인기 상품 목록
+     */
+    public List<Long> getPopularPurchaseItems(int topN) {
+        return getZsetPurchaseItems(topN).stream()
+                .map(ZSetOperations.TypedTuple::getValue)
+                .map(Long::parseLong)
+                .toList();
+    }
+
+    private Set<ZSetOperations.TypedTuple<String>> getZsetPurchaseItems(int topN) {
+        return redisTemplate.opsForZSet().reverseRangeWithScores(PURCHASE_ITEM_ZSET_KEY, 0, topN - 1);
+    }
+
+    /**
+     * 모든 상품 구매 카운트 삭제
+     */
+    public void deleteAllPurchaseItems() {
+        redisTemplate.delete(PURCHASE_ITEM_ZSET_KEY);
     }
 }

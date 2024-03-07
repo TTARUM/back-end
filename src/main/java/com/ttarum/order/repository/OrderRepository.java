f1c976df3b2093317776cc9358d3bf28b034ddae
salbyul <kpast96@gmail.com>
2024-03-07 17:23:35 +0900

    Add new query to find OrderItemSummary.

--- src/main/java/com/ttarum/order/repository/OrderRepository.java
+++ src/main/java/com/ttarum/order/repository/OrderRepository.java
@@ -1,7 +1,32 @@
 package com.ttarum.order.repository;
 
 import com.ttarum.order.domain.Order;
+import com.ttarum.order.dto.response.OrderItemSummary;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @SuppressWarnings("SqlResolve")
+    @Query(value = """
+            SELECT i.id AS itemId,
+            i.item_image_url AS itemImageUrl,
+            i.name AS itemName,
+            i.price AS itemPrice,
+            oi.amount AS amount,
+            exists(
+            SELECT 1 FROM Review r WHERE r.order_id = oi.order_id AND r.item_id = oi.item_id
+            ) AS hasReview
+            FROM `Order` o
+            LEFT JOIN Order_Item oi
+            ON o.id = oi.order_id
+            LEFT JOIN Item i
+            ON i.id = oi.item_id
+            WHERE o.id = :orderId
+            """, nativeQuery = true)
+    List<OrderItemSummary> findOrderItemListByOrderId(@Param("orderId") long orderId, Pageable pageable);
 }

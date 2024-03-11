9adf0b804af34fa52e0d93c1d5f11ba0627313dd
salbyul <kpast96@gmail.com>
2024-03-11 13:43:54 +0900

    Add message field.

--- src/main/java/com/ttarum/order/domain/PaymentMethod.java
+++ src/main/java/com/ttarum/order/domain/PaymentMethod.java
@@ -1,5 +1,16 @@
 package com.ttarum.order.domain;
 
 public enum PaymentMethod {
-    CREDIT_CARD
+    CREDIT_CARD("신용카드");
+
+    private final String message;
+
+    PaymentMethod(final String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
 }

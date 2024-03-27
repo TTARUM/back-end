875d2a7f5c8b169fb102ade91df7baaaa0dd9bf5
salbyul <kpast96@gmail.com>
2024-03-27 14:47:58 +0900

    implement a Coupon domain.

---
+++ src/main/java/com/ttarum/member/domain/coupon/Coupon.java
@@ -0,0 +1,23 @@
+package com.ttarum.member.domain.coupon;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@Table(name = "coupon")
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private CouponStrategy couponStrategy;
+
+    private int value;
+}

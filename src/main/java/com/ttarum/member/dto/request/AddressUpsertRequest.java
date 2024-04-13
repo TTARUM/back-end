1e3751608b48fc0b0e780e993e38fb9f124500ff
koowin42@gmail.com <koowin42@gmail.com>
2024-04-13 14:32:19 +0900

    TTAR-0122 Update dto `AddressUpsertRequest`

--- src/main/java/com/ttarum/member/dto/request/AddressUpsertRequest.java
+++ src/main/java/com/ttarum/member/dto/request/AddressUpsertRequest.java
@@ -8,6 +8,16 @@ import lombok.Getter;
 @AllArgsConstructor
 @Schema(name = "AddressUpsertRequest", description = "배송지 추가/수정 요청 DTO")
 public class AddressUpsertRequest {
+    @Schema(description = "배송지 별칭", example = "집")
+    private final String addressAlias;
+    @Schema(description = "수령인", example = "홍길동")
+    private final String recipient;
     @Schema(description = "배송지 주소", example = "서울시 강남구 역삼동 123-456")
     private final String address;
+    @Schema(description = "상세 주소", example = "아파트 123동 456호")
+    private final String detailAddress;
+    @Schema(description = "전화번호", example = "010-1234-5678")
+    private final String phoneNumber;
+    @Schema(description = "기본 배송지 여부", example = "true")
+    private final boolean isDefault;
 }

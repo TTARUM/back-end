61117c8983df130ca0f833dd71aa9bf998f5c4b6
koowin42@gmail.com <koowin42@gmail.com>
2024-04-13 14:31:57 +0900

    TTAR-0122 Update `Address` domain

--- src/main/java/com/ttarum/member/domain/Address.java
+++ src/main/java/com/ttarum/member/domain/Address.java
@@ -16,11 +16,26 @@ public class Address {
     @Column(name = "id", nullable = false, columnDefinition = "int")
     private Long id;
 
+    @Column(name = "address_alias", nullable = false, length = 45)
+    private String addressAlias;
+
+    @Column(name = "recipient", nullable = false, length = 20)
+    private String recipient;
+
     @Setter
     @Column(name = "address", nullable = false, length = 100)
     private String address;
 
+    @Column(name = "detail_address", nullable = false, length = 100)
+    private String detailAddress;
+
+    @Column(name = "phone_number", nullable = false, length = 15)
+    private String phoneNumber;
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "member_id", nullable = false, columnDefinition = "int")
     private Member member;
+
+    @Column(name = "is_default", nullable = false)
+    boolean isDefault;
 }

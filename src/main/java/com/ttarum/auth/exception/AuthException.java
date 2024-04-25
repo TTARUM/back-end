154e3d2f9994a3b3072cfa4d37f6f516e10ddd34
koowin42@gmail.com <koowin42@gmail.com>
2024-04-26 03:31:19 +0900

    TTAR-0118 Add new AuthException:DELETED_MEMBER

--- src/main/java/com/ttarum/auth/exception/AuthException.java
+++ src/main/java/com/ttarum/auth/exception/AuthException.java
@@ -8,6 +8,7 @@ public class AuthException extends TtarumException {
     private static final String USER_NOT_FOUND = "사용자를 찾을 수 없습니다.";
     private static final String INVALID_PASSWORD = "비밀번호가 일치하지 않습니다.";
     private static final String INVALID_TOKEN = "유효하지 않은 토큰입니다.";
+    private static final String DELETED_MEMBER = "탈퇴한 회원입니다.";
 
     public AuthException(final HttpStatusCode status, final String message) {
         super(status, message);
@@ -23,5 +24,9 @@ public class AuthException extends TtarumException {
 
     public static AuthException InvalidToken() {
         return new AuthException(HttpStatus.UNAUTHORIZED, INVALID_TOKEN);
+    }
+
+    public static AuthException DeletedMember() {
+        return new AuthException(HttpStatus.UNAUTHORIZED, DELETED_MEMBER);
     }
 }

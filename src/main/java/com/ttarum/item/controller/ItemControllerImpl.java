90560c329777ebeea5d7571522f6ceb2e72fcd8c
koowin42@gmail.com <koowin42@gmail.com>
2024-04-28 13:02:13 +0900

    TTAR-0137 Delete ItemControllerImpl

--- src/main/java/com/ttarum/item/controller/ItemControllerImpl.java
+++ src/main/java/com/ttarum/item/controller/ItemControllerImpl.java
@@ -9,9 +9,16 @@ import com.ttarum.item.dto.response.*;
 import com.ttarum.item.domain.redis.PopularItem;
 import com.ttarum.item.service.ItemService;
 import com.ttarum.item.service.RedisService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +29,8 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/items")
-public class ItemControllerImpl implements ItemController {
+@Tag(name = "item", description = "제품")
+public class ItemControllerImpl {
 
     private static final int ITEM_DEFAULT_SIZE_PER_PAGE = 9;
     private static final int ITEM_SIMILAR_PRICE_DEFAULT_SIZE_PER_PAGE = 7;
@@ -30,10 +38,26 @@ public class ItemControllerImpl implements ItemController {
     private final ItemService itemService;
     private final RedisService redisService;
 
-    @Override
+    /**
+     * 특정 제품의 상세정보 조회 메서드
+     *
+     * @param itemId 제품의 Id 값
+     * @return 제품의 상세정보
+     */
+    @Operation(summary = "특정 제품의 상세정보 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "존재하지 않는 제품")
+    })
+    @Parameters(value = {
+            @Parameter(name = "itemId", description = "제품의 아이디", example = "1", required = true),
+            @Parameter(name = "useSearch", description = "검색을 이용해 제품을 조회하는지 여부 (없을 시 false 적용)", example = "true")
+    })
     @GetMapping("/{itemId}")
-    public ResponseEntity<ItemDetailResponse> getDetail(@PathVariable final long itemId,
-                                                        @RequestParam(required = false, defaultValue = "false") final boolean useSearch) {
+    public ResponseEntity<ItemDetailResponse> getDetail(
+            @PathVariable final long itemId,
+            @RequestParam(required = false, defaultValue = "false") final boolean useSearch
+    ) {
         ItemDetailResponse response = itemService.getItemDetail(itemId);
         if (useSearch) {
             redisService.incrementSearchKeywordCount(response.getName(), itemId);
@@ -41,7 +65,24 @@ public class ItemControllerImpl implements ItemController {
         return ResponseEntity.ok(response);
     }
 
-    @Override
+    /**
+     * 요약된 제품 정보에 대한 검색 메서드
+     *
+     * @param query 검색어
+     * @param user  로그인한 사용자 여부를 확인하기 위한 객체
+     * @param page  페이지 넘버
+     * @param size  아이템 사이즈
+     * @return 검색된 제품 목록
+     */
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    @Operation(summary = "이름으로 제품 검색")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponse(responseCode = "200", description = "성공")
+    @Parameters(value = {
+            @Parameter(name = "query", description = "검색어"),
+            @Parameter(name = "page", description = "페이지 넘버 (기본 값 0)", example = "1"),
+            @Parameter(name = "size", description = "한 페이지당 제품 수 (기본 값 9개)", example = "9")
+    })
     @GetMapping("/list")
     public ResponseEntity<ItemSummaryResponse> getSummary(
             @RequestParam(required = false) final String query,
@@ -60,16 +101,46 @@ public class ItemControllerImpl implements ItemController {
         return ResponseEntity.ok(response);
     }
 
-    @Override
+    /**
+     * 인기 검색어 조회
+     *
+     * @param number 조회할 인기 검색어 개수
+     * @return 인기 검색어 목록
+     */
+    @Operation(summary = "인기 검색어 조회")
+    @ApiResponse(responseCode = "200", description = "성공")
+    @Parameter(name = "number", description = "조회할 인기 검색어 개수 (기본 값 5)", example = "5")
     @GetMapping("/popular-list")
-    public ResponseEntity<PopularItemResponse> getPopularItemList(@RequestParam(required = false, defaultValue = "5") final int number) {
+    public ResponseEntity<PopularItemResponse> getPopularItemList(
+            @RequestParam(required = false, defaultValue = "5") final int number
+    ) {
         List<PopularItem> popularSearchKeywords = redisService.getPopularSearchKeywords(number);
         return ResponseEntity.ok(new PopularItemResponse(popularSearchKeywords));
     }
 
-    @Override
+    /**
+     * 가격대가 비슷한 술 조회
+     *
+     * @param user  로그인한 사용자 여부를 확인하기 위한 객체
+     * @param price 가격대
+     * @return 조회된 제품 리스트
+     */
+    @Operation(summary = "가격대가 비슷한 술 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "조회 실패")
+    })
+    @Parameters(value = {
+            @Parameter(name = "price", description = "가격", example = "17000"),
+            @Parameter(name = "page", description = "페이지 넘버 (기본 값 0)", example = "1"),
+            @Parameter(name = "size", description = "페이지당 개수 (기본 값 7)", example = "7")
+    })
     @GetMapping("/similar-price")
-    public ResponseEntity<ItemSimilarPriceResponse> getSummaryWithSimilarPriceRange(@VerificationUser final Optional<LoggedInUser> user, final int price, final Optional<Integer> page, final Optional<Integer> size) {
+    public ResponseEntity<ItemSimilarPriceResponse> getSummaryWithSimilarPriceRange(
+            final int price, final Optional<Integer> page,
+            final Optional<Integer> size,
+            @Parameter(hidden = true) @VerificationUser final Optional<LoggedInUser> user
+    ) {
 
         ItemSimilarPriceResponse response;
         PageRequest pageRequest = PageRequest.of(page.orElse(0), size.orElse(ITEM_SIMILAR_PRICE_DEFAULT_SIZE_PER_PAGE));
@@ -81,9 +152,29 @@ public class ItemControllerImpl implements ItemController {
         return ResponseEntity.ok(response);
     }
 
-    @Override
+    /**
+     * 카테고리 인기상품 조회
+     *
+     * @param user     로그인한 회원
+     * @param category 카테고리 이름
+     * @param page     페이지 넘버
+     * @param size     페이지당 조회할 제품의 수
+     * @return 조회된 인기상품
+     */
+    @Operation(summary = "카테고리 인기상품 조회")
+    @ApiResponse(responseCode = "200", description = "조회 성공")
+    @Parameters(value = {
+            @Parameter(name = "category", description = "카테고리 이름", example = "red"),
+            @Parameter(name = "page", description = "페이지 넘버 (기본값 0)", example = "1"),
+            @Parameter(name = "size", description = "한 페이지당 제품 수 (기본 값 7)", example = "7")
+    })
     @GetMapping("/popular-in-category")
-    public ResponseEntity<PopularItemInCategoryResponse> getPopularItemSummaryListInCategory(@VerificationUser final Optional<LoggedInUser> user, final String category, final Optional<Integer> page, final Optional<Integer> size) {
+    public ResponseEntity<PopularItemInCategoryResponse> getPopularItemSummaryListInCategory(
+            final String category,
+            final Optional<Integer> page,
+            final Optional<Integer> size,
+            @Parameter(hidden = true) @VerificationUser final Optional<LoggedInUser> user
+    ) {
         PageRequest pageRequest = PageRequest.of(page.orElse(0), size.orElse(ITEM_POPULAR_IN_CATEGORY_DEFAULT_SIZE_PER_PAGE));
         PopularItemInCategoryResponse response;
         if (user.isPresent()) {
@@ -94,9 +185,28 @@ public class ItemControllerImpl implements ItemController {
         return ResponseEntity.ok(response);
     }
 
-    @Override
+    /**
+     * 카테고리별 제품 조회
+     *
+     * @param user     로그인한 회원
+     * @param category 카테고리 이름
+     * @param page     페이지 넘버
+     * @param size     페이지당 조회할 제품의 수
+     * @return 조회된 제품 리스트
+     */
+    @Operation(summary = "카테고리별 제품 조회")
+    @ApiResponse(responseCode = "200", description = "조회 성공")
+    @Parameters(value = {
+            @Parameter(name = "page", description = "페이지 넘버 (기본값 0)", example = "0"),
+            @Parameter(name = "size", description = "한 페이지당 제품 수 (기본 값 9)", example = "9")
+    })
     @GetMapping("/category")
-    public ResponseEntity<ItemSummaryResponse> getSummaryByCategory(@VerificationUser final Optional<LoggedInUser> user, @PathVariable final String category, final Optional<Integer> page, final Optional<Integer> size) {
+    public ResponseEntity<ItemSummaryResponse> getSummaryByCategory(
+            final String category,
+            final Optional<Integer> page,
+            final Optional<Integer> size,
+            @Parameter(hidden = true) @VerificationUser final Optional<LoggedInUser> user
+    ) {
         PageRequest pageRequest = PageRequest.of(page.orElse(0), size.orElse(ITEM_DEFAULT_SIZE_PER_PAGE));
         ItemSummaryResponse response;
         if (user.isPresent()) {

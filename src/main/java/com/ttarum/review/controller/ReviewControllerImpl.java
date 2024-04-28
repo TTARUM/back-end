1283678532c47e5aa836acdee9e1564bfef5c3fc
koowin42@gmail.com <koowin42@gmail.com>
2024-04-28 13:23:41 +0900

    TTAR-0137 Delete ReviewControllerImpl

--- src/main/java/com/ttarum/review/controller/ReviewControllerImpl.java
+++ src/main/java/com/ttarum/review/controller/ReviewControllerImpl.java
@@ -8,6 +8,12 @@ import com.ttarum.review.dto.response.ReviewResponse;
 import com.ttarum.review.dto.response.ReviewUpdateResponse;
 import com.ttarum.review.service.ReviewImageService;
 import com.ttarum.review.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -26,55 +32,136 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/reviews")
-public class ReviewControllerImpl implements ReviewController {
+@Tag(name = "review", description = "리뷰")
+public class ReviewControllerImpl {
 
     private static final int PAGE_DEFAULT_SIZE = 10;
 
     private final ReviewService reviewService;
     private final ReviewImageService reviewImageService;
 
+    /**
+     * 특정 제품에 대한 리뷰 조회
+     *
+     * @param itemId 특정 제품의 Id 값
+     * @param page   페이지 넘버
+     * @param size   페이지당 반환되는 리뷰의 개수
+     * @return 리뷰 목록
+     */
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    @Operation(summary = "특정 제품에 대한 리뷰 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 제품 조회")
+    })
+    @Parameters(value = {
+            @Parameter(name = "itemId", description = "제품의 PK 값", example = "1"),
+            @Parameter(name = "page", description = "페이지 넘버 (기본 값 0)", example = "1"),
+            @Parameter(name = "size", description = "한 페이지당 리뷰 수 (기본 값 10개)", example = "5")
+
+    })
     @GetMapping
-    @Override
-    public ResponseEntity<List<ReviewResponse>> getReviewResponseList(@RequestParam final long itemId,
-                                                                      final Optional<Integer> page,
-                                                                      final Optional<Integer> size) {
+    public ResponseEntity<List<ReviewResponse>> getReviewResponseList(
+            @RequestParam final long itemId,
+            final Optional<Integer> page,
+            final Optional<Integer> size
+    ) {
         PageRequest pageRequest = PageRequest.of(page.orElse(0), size.orElse(PAGE_DEFAULT_SIZE));
         List<ReviewResponse> list = reviewService.getReviewResponseList(itemId, pageRequest);
         return ResponseEntity.ok(list);
     }
 
+    /**
+     * 특정 리뷰 제거
+     *
+     * @param reviewId 제거할 리뷰의 Id 값
+     * @param user     로그인한 회원
+     * @return 빈 응답
+     */
+    @Operation(summary = "특정 리뷰 제거")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "제거 성공"),
+            @ApiResponse(responseCode = "400", description = "제거 실패")
+    })
+    @Parameter(name = "reviewId", description = "제거할 리뷰의 ID 값", example = "1")
     @DeleteMapping("/{reviewId}")
-    @Override
-    public ResponseEntity<Void> deleteReview(@PathVariable final long reviewId,
-                                             @AuthenticationPrincipal final CustomUserDetails user) {
+    public ResponseEntity<Void> deleteReview(
+            @PathVariable final long reviewId,
+            @AuthenticationPrincipal final CustomUserDetails user
+    ) {
         reviewService.deleteReview(reviewId, user.getId());
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * 리뷰 업데이트
+     *
+     * @param reviewId 업데이트할 리뷰의 Id 값
+     * @param request  업데이트될 정보가 담긴 객체
+     * @param user     로그인한 회원
+     * @return 빈 응답
+     */
+    @Operation(summary = "리뷰 업데이트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "리뷰 업데이트 성공"),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 리뷰")
+    })
+    @Parameters(value = {
+            @Parameter(name = "reviewId", description = "리뷰의 Id값", example = "1", required = true)
+    })
     @PutMapping("/{reviewId}")
-    @Override
-    public ResponseEntity<Void> updateReview(@PathVariable final long reviewId,
-                                             @RequestBody final ReviewUpdateRequest request,
-                                             @AuthenticationPrincipal final CustomUserDetails user) {
+    public ResponseEntity<Void> updateReview(
+            @PathVariable final long reviewId,
+            @RequestBody final ReviewUpdateRequest request,
+            @AuthenticationPrincipal final CustomUserDetails user
+    ) {
         reviewService.updateReview(reviewId, request, user.getId());
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * 리뷰 업데이트를 위한 데이터 조회
+     *
+     * @param reviewId 조회할 리뷰의 Id 값
+     * @param user     로그인한 회원
+     * @return 리뷰 데이터
+     */
+    @Operation(summary = "리뷰 업데이트를 위한 데이터 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "조회 실패")
+    })
+    @Parameter(name = "reviewId", description = "조회할 리뷰의 Id 값", example = "1")
     @GetMapping("/{reviewId}/update")
-    @Override
-    public ResponseEntity<ReviewUpdateResponse> updateReview(@PathVariable final long reviewId,
-                                                             @AuthenticationPrincipal final CustomUserDetails user) {
+    public ResponseEntity<ReviewUpdateResponse> updateReview(
+            @PathVariable final long reviewId,
+            @AuthenticationPrincipal final CustomUserDetails user
+    ) {
         ReviewUpdateResponse reviewUpdateResponse = reviewService.getReviewForUpdating(user.getId(), reviewId);
         return ResponseEntity.ok(reviewUpdateResponse);
     }
 
+    /**
+     * 리뷰 작성
+     *
+     * @param user              로그인한 사용자
+     * @param multipartFileList 이미지 파일 리스트
+     * @param request           생성될 리뷰의 데이터
+     * @return 생성된 리뷰의 Id 값
+     */
+    @Operation(summary = "리뷰 작성")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "리뷰 작성 성공"),
+            @ApiResponse(responseCode = "400", description = "리뷰 작성 실패")
+    })
     @Transactional
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @Override
-    public ResponseEntity<ReviewCreationResponse> createReview(@AuthenticationPrincipal final CustomUserDetails user,
-                                                               @RequestPart(name = "images", required = false) final List<MultipartFile> multipartFileList,
-                                                               @RequestPart(name = "reviewCreationRequest") final ReviewCreationRequest reviewCreationRequest) {
-        long reviewId = reviewService.createReview(user.getId(), reviewCreationRequest);
+    public ResponseEntity<ReviewCreationResponse> createReview(
+            @RequestPart(name = "images", required = false) final List<MultipartFile> multipartFileList,
+            @RequestPart(name = "reviewCreationRequest") final ReviewCreationRequest request,
+            @AuthenticationPrincipal final CustomUserDetails user
+    ) {
+        long reviewId = reviewService.createReview(user.getId(), request);
         if (Objects.nonNull(multipartFileList) && !multipartFileList.isEmpty()) {
             reviewImageService.saveImageList(reviewId, multipartFileList);
         }

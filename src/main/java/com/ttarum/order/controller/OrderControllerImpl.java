49f8639836cf49238d0cdf8378e9a78fbe836dd9
koowin42@gmail.com <koowin42@gmail.com>
2024-04-28 13:16:56 +0900

    TTAR-0137 Delete OrderController

--- src/main/java/com/ttarum/order/controller/OrderControllerImpl.java
+++ src/main/java/com/ttarum/order/controller/OrderControllerImpl.java
@@ -4,6 +4,11 @@ import com.ttarum.auth.domain.CustomUserDetails;
 import com.ttarum.order.dto.response.OrderDetailResponse;
 import com.ttarum.order.dto.response.summary.OrderSummaryListResponse;
 import com.ttarum.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -15,23 +20,51 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/orders")
-public class OrderControllerImpl implements OrderController {
+@Tag(name = "order", description = "주문")
+public class OrderControllerImpl {
 
     private final OrderService orderService;
 
-    @Override
+    /**
+     * 주문 내역 목록 조회
+     *
+     * @param user 로그인한 회원
+     * @param page 페이지 넘버
+     * @param size 페이지당 주문 내역 개수
+     * @return 주문 내역 목록
+     */
+    @Operation(summary = "주문 내역 목록 조회")
+    @ApiResponse(responseCode = "200", description = "성공")
+    @Parameters(value = {
+            @Parameter(name = "page", description = "페이지 넘버 (기본 값 0)", example = "0"),
+            @Parameter(name = "size", description = "페이지당 주문 내역 개수 (기본 값 5)", example = "5")
+    })
     @GetMapping("/list")
-    public ResponseEntity<OrderSummaryListResponse> getOrderList(@AuthenticationPrincipal final CustomUserDetails user,
-                                                                 @RequestParam(required = false, defaultValue = "0") int page,
-                                                                 @RequestParam(required = false, defaultValue = "5") int size) {
+    public ResponseEntity<OrderSummaryListResponse> getOrderList(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "5") int size,
+            @AuthenticationPrincipal final CustomUserDetails user
+    ) {
         PageRequest pageRequest = PageRequest.of(page, size);
         OrderSummaryListResponse response = orderService.getOrderSummaryList(user.getId(), pageRequest);
         return ResponseEntity.ok(response);
     }
 
-    @Override
+    /**
+     * 주문 조회
+     *
+     * @param user    로그인한 회원
+     * @param orderId 조회할 주문의 Id 값
+     * @return 주문의 세부사항
+     */
+    @Operation(summary = "주문 조회")
+    @ApiResponse(responseCode = "200", description = "성공")
+    @Parameter(name = "orderId", description = "조회할 주문의 Id 값", example = "1")
     @GetMapping("/{orderId}")
-    public ResponseEntity<OrderDetailResponse> getOrderDetail(@AuthenticationPrincipal final CustomUserDetails user, @PathVariable final long orderId) {
+    public ResponseEntity<OrderDetailResponse> getOrderDetail(
+            @PathVariable final long orderId,
+            @AuthenticationPrincipal final CustomUserDetails user
+    ) {
         OrderDetailResponse orderDetailResponse = orderService.getOrderDetail(user.getId(), orderId);
         return ResponseEntity.ok(orderDetailResponse);
     }

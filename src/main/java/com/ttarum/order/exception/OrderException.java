0704f0c776ccbe5194c49aedb969d478a2e5c36f
koowin42@gmail.com <koowin42@gmail.com>
2024-05-04 18:52:41 +0900

    TTAR-0141 Add itemNotFound exception

--- src/main/java/com/ttarum/order/exception/OrderException.java
+++ src/main/java/com/ttarum/order/exception/OrderException.java
@@ -7,9 +7,14 @@ import org.springframework.http.HttpStatusCode;
 public class OrderException extends TtarumException {
 
     private static final String ORDER_NOT_FOUND_MESSAGE = "주문을 찾을 수 없습니다.";
+    private static final String ITEM_NOT_FOUND_MESSAGE = "주문하신 상품을 찾을 수 없습니다.";
 
     public static OrderException notFound() {
         return new OrderException(HttpStatus.NOT_FOUND, ORDER_NOT_FOUND_MESSAGE);
+    }
+
+    public static OrderException itemNotFound() {
+        return new OrderException(HttpStatus.NOT_FOUND, ITEM_NOT_FOUND_MESSAGE);
     }
 
     public OrderException(final HttpStatusCode status, final String message) {

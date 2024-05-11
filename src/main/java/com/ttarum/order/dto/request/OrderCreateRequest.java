78ca3f944713e0cf1ca9ba5e3696e23524bac6aa
koowin42@gmail.com <koowin42@gmail.com>
2024-05-11 14:53:05 +0900

    TTAR-0163 Add fields ``couponId` and `totalPrice` to OrderCreateRequest

--- src/main/java/com/ttarum/order/dto/request/OrderCreateRequest.java
+++ src/main/java/com/ttarum/order/dto/request/OrderCreateRequest.java
@@ -6,26 +6,49 @@ import com.ttarum.order.domain.Order;
 import com.ttarum.order.domain.OrderStatus;
 import com.ttarum.order.domain.PaymentMethod;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 @Schema(name = "OrderCreateRequest", description = "주문 생성 요청 DTO")
 public class OrderCreateRequest {
     private static final int DEFAULT_DELIVERY_FEE = 3000;
 
+    // TODO: 주문 요청사항 필수 값인지 확인하고 필요시 스키마 변경하기
     @Schema(description = "주문 요청 사항", example = "부재 시 경비실에 맡겨주세요")
-    private String comment;
+    private final String comment;
+
+    @NotBlank(message = "전화번호는 필수 값입니다.")
     @Schema(description = "전화번호", example = "010-1234-5678")
-    private String phoneNumber;
+    private final String phoneNumber;
+
+    @NotBlank(message = "배송지 주소는 필수 값입니다.")
     @Schema(description = "배송지 주소", example = "서울시 강남구 역삼동 123-456")
-    private String address;
+    private final String address;
+
+    @NotBlank(message = "수령인은 필수 값입니다.")
     @Schema(description = "수령인", example = "홍길동")
-    private String recipient;
+    private final String recipient;
+
+    @Size(min = 1, message = "주문 상품은 최소 1개 이상이어야 합니다.")
     @Schema(description = "주문 상품 목록")
     @JsonProperty("orderItems")
-    private List<OrderItemRequest> orderItemRequests;
+    private final List<OrderItemRequest> orderItemRequests;
+
+    @Schema(description = "쿠폰 ID", example = "1")
+    private Long couponId;
+
+    @NotNull(message = "총 주문 금액은 필수 값입니다.")
+    @Positive(message = "총 주문 금액은 0보다 커야 합니다.")
+    @Schema(description = "총 주문 금액", example = "30000")
+    private final Integer totalPrice;
 
     public Order toOrderEntity(long price, Member member) {
         return Order.builder()
@@ -50,8 +73,10 @@ public class OrderCreateRequest {
         sb.append("recipient:").append(recipient).append("\n");
         sb.append("orderItems:\n");
         for (OrderItemRequest orderItemRequest : orderItemRequests) {
-            sb.append(orderItemRequest.getItemId()).append(" - ").append(orderItemRequest.getQuantity()).append("\n");
+            sb.append(orderItemRequest.getItemId()).append("\t - ").append(orderItemRequest.getQuantity()).append("\n");
         }
+        sb.append("couponId:").append(couponId).append("\n");
+        sb.append("totalPrice:").append(totalPrice).append("\n");
         return sb.toString();
     }
 }

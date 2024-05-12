af4be2004019c597bd6ba8eef997ddb68fcefaf2
salbyul <kpast96@gmail.com>
2024-05-12 20:00:37 +0900

    Add a feature to find email.

--- src/main/java/com/ttarum/member/repository/NormalMemberRepository.java
+++ src/main/java/com/ttarum/member/repository/NormalMemberRepository.java
@@ -2,6 +2,8 @@ package com.ttarum.member.repository;
 
 import com.ttarum.member.domain.NormalMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,4 +11,7 @@ public interface NormalMemberRepository extends JpaRepository<NormalMember, Long
     Optional<NormalMember> findNormalMemberByLoginId(String loginId);
 
     boolean existsByEmail(String email);
+
+    @Query("SELECT nm FROM NormalMember nm WHERE nm.email = :email")
+    Optional<NormalMember> findNormalMemberByEmail(@Param("email") String email);
 }
--- src/main/java/com/ttarum/member/service/RedisAuthService.java
+++ src/main/java/com/ttarum/member/service/RedisAuthService.java
@@ -1,6 +1,8 @@
 package com.ttarum.member.service;
 
+import com.ttarum.member.mail.EmailVerification;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
@@ -11,25 +13,37 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public class RedisAuthService {
 
-    private final StringRedisTemplate redisTemplate;
+    private final StringRedisTemplate stringTemplate;
+    private final RedisTemplate<String, EmailVerification> redisTemplate;
 
     public String getData(final String key) {
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        ValueOperations<String, String> valueOperations = stringTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public EmailVerification getEmailVerification(final String key) {
+        ValueOperations<String, EmailVerification> valueOperations = redisTemplate.opsForValue();
         return valueOperations.get(key);
     }
 
     public void setData(final String key, final String value) {
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        ValueOperations<String, String> valueOperations = stringTemplate.opsForValue();
         valueOperations.set(key, value);
     }
 
     public void setDataExpire(final String key, final String value, final long duration) {
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        ValueOperations<String, String> valueOperations = stringTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public void setDataExpire(final String key, final EmailVerification value, final long duration) {
+        ValueOperations<String, EmailVerification> valueOperations = redisTemplate.opsForValue();
         Duration expireDuration = Duration.ofSeconds(duration);
         valueOperations.set(key, value, expireDuration);
     }
 
     public void deleteData(final String key) {
-        redisTemplate.delete(key);
+        stringTemplate.delete(key);
     }
 }
